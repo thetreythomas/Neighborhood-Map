f47105c83802b29a8fdc9e27f3fac0ce998b0765
Trey <trizney@austin.rr.com>
2016-10-07 02:52:24 -0500

    Nuked and started all over
    This has the latest work of what I have been able to redo

--- js/app.js
+++ js/app.js
@@ -1,199 +1,213 @@
-// Model
-
-// Google Map and Markers
-var map;
-var myLatLong = {lat: 30.2672, lng: -97.7431};
-
-// function initMap() {
-//     map = new google.maps.Map(document.getElementById('map'), {
-//       center: myLatLong,
-//       zoom: 12
-//     });
-
-//     var marker = new google.maps.Marker({
-//         map: map,
-//         position: myLatLong,
-//         animation: google.maps.Animation.DROP,
-//         title: "This is Austin, y'all!"
-//     });
-
-//     var infowindow = new google.maps.InfoWindow({
-//         content: '<p>Marker Location:' + marker.getPosition() + '</p>'
-//     });
-
-//     google.maps.event.addListener(marker, 'click', function() {
-//         infowindow.open(map, marker);
-//     });
-// }
-
-
 // My Markers
 var myMarkers = [
     {
-        title: "K1 Speed",
+        name: "K1 Speed",
         lat: 30.384619,
         lng: -97.720848,
         street: "2500 McHale Ct.",
         city: "Austin, TX 78758",
-        phone: "(512) 271-5475",
+        //phone: "(512) 271-5475",
         url: "https://www.k1speed.com/austin-location.html"
     },
     {
-        title: "Topgolf",
+        name: "Topgolf",
         lat: 30.398064,
         lng: -97.717714,
         street: "2700 Esperanza Crossing",
         city: "Austin, TX 78758",
-        phone: "(512) 222-5950",
+        //phone: "(512) 222-5950",
         url: "http://http://topgolf.com/us/austin/"
     },
     {
-        title: "Alamo Drafthouse - Village",
+        name: "Alamo Drafthouse - Village",
         lat: 30.360031,
         lng: -97.734853,
         street: "2700 W Anderson Ln",
         city: "Austin, TX 78757",
-        phone: "(512) 861-7030",
+        //phone: "(512) 861-7030",
         url: "https://drafthouse.com/theater/village"
     },
     {
-        title: "The Common Interest - Karaoke Bar & Grill",
+        name: "The Common Interest - Karaoke Bar & Grill",
         lat: 30.366242,
         lng: -97.729238,
         street: "8400 Burnet Rd",
         city: "Austin, TX 78757",
-        phone: "(512) 453-6796",
+        //phone: "(512) 453-6796",
         url: "http://www.ciaustin.com/"
     },
     {
-        title: "GT Distributors - Gun Store",
+        name: "GT Distributors - Gun Store",
         lat: 30.391606,
         lng: -97.719653,
         street: "2545 Brockton Dr. #100",
         city: "Austin, TX 78758",
-        phone: "(512) 451-8298",
+        //phone: "(512) 451-8298",
         url: "https://www.gtdist.com/"
     },
     {
-        title: "Fry's Electronics",
+        name: "Fry's Electronics",
         lat: 30.422928,
         lng: -97.700197,
         street: "12707 N Mopac Expy",
         city: "Austin, TX 78727",
-        phone: "(512) 733-7000",
+        //phone: "(512) 733-7000",
         url: "http://www.frys.com/ac/storeinfo/austin-location-frys-electronics-hours-maps-directions"
     },
     {
-        title: "Opal Divine's Marina",
+        name: "Opal Divine's Marina",
         lat: 30.423585,
         lng: -97.702114,
         street: "12709 N Mopac Expy",
         city: "Austin, TX 78727",
-        phone: "(512) 733-5353",
+        //phone: "(512) 733-5353",
         url: "http://www.opaldivines.com/"
     },
     {
-        title: "Walnut Creek Metroploitian Park",
+        name: "Walnut Creek Metroploitian Park",
         lat: 30.401127,
         lng: -97.685960,
         street: "12138 N Lamar Blvd",
         city: "AUstin, TX 78753",
-        phone: "(512) 974-6700",
+        //phone: "(512) 974-6700",
         url: "http://www.austintexas.gov/page/park-directory#w"
     }
 ];
 
+// View Model
 
-var markerItem = function(data) {
+function ViewModel () {
+console.log("ViewModel Start");
+
     var self = this;
 
-    this.title = data.title;
-    this.lat = data.lat;
-    this.lng = data.lng;
-    this.street = data.street;
-    this.city = data.city;
-    this.phone = data.phone;
-    this.url = data.url
-    this.show = ko.observable(true);
+    // Location Constructor Function
+    var Location = function(data) {
+        var that = this;
+        console.log("Location "+data.name+" set");
+        this.name = ko.observable(data.name);
+        console.log(this.name());
+        this.lat = ko.observable(data.lat);
+        console.log(this.lat());
+        this.lng = ko.observable(data.lng);
+        console.log(this.lng());
+        this.street = ko.observable(data.street);
+        console.log(this.street());
+        this.city = ko.observable(data.city);
+        console.log(this.city());
+        this.phone = ko.observable(""); // Will be retrieved from Foursquare API
+        console.log(this.phone());
+        this.url = ko.observable("");  // Will be retrieved from Foursquare API
+        console.log(this.url());
+        this.show = ko.observable(true);
+        console.log(this.show());
+        //Items added from Foursquare API
+        // this.url = '';
+        //this.facebookName = ko.observable('');
+        //this.categoryName = ko.observable('');
 
-    this.marker = new google.maps.Marker({
-        position: new google.maps.LatLng(data.lat, data.lng),
-        map: map,
-        title: data.title
+        // Foursquare API Information
+        var clientID = "D2GUCKJP4VMFZBHN3ZAL3WX2LNFDLARDD4HS5FG3IAJBRV4B";
+        var clientSecret = "2JUH3GZ3D1XDCG5R0WK5BPZHYAKYLNUCGXP4DY2ZOJWIIWO0";
+
+        // Foursquare API Call
+        // https://developer.foursquare.com/docs/venues/search
+        var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat() +
+        ',' + this.lng() + '&client_id=' + clientID + '&client_secret=' + clientSecret +
+        '&v=20160118' + '&query=' + this.name();
+        console.log(foursquareURL);
+
+        // .getJSON from each searched location
+        // http://api.jquery.com/jquery.getjson/
+        $.getJSON(foursquareURL, function(data) {
+            //console.log(data.repsonse.venues[0]);
+
+            var result = data.response.venues[0];
+            if (typeof(result.url === 'undefined')) {
+                that.url("");
+            } else {
+                that.url(result.url);
+            }
+            console.log(that.url() + " This is the URL from Foursquare API");
+            that.phone(result.contact.formattedPhone);
+            console.log(that.phone() + " This is the formatted phone number from Foursquare API");
+
+            // that.facebookName(result.contact.facebookUsername);
+            // console.log(that.facebookName() + " This is the Facebook Name from Foursquare API")
+            // that.categoryName(result.categories.name);
+            // console.log(that.categoryName() + " This is the Category type from Foursquare API");
+
+        }).fail(function (){
+            alert("Danger, Will Robinson! Danger!");
+        });
+
+
+        // Google Maps API info window information
+        // https://developers.google.com/maps/documentation/javascript/infowindows
+        var contentString = '<div><b>' + this.name() + '</b></div>' +
+                            '<div>'+ this.street() + '</div>' +
+                            '<div>'+ that.phone() + '</div>' +
+                            '<div>'+ this.lat() + '</div>';
+
+        var infoWindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        this.marker = new google.maps.Marker({
+            position: new google.maps.LatLng(data.lat, data.lng),
+            map: map,
+            title: data.name
+        });
+
+        this.marker.setMap(map);
+
+        this.marker.addListener('click', function() {
+            infoWindow.open(map, this);
+        });
+    }; // End of Location Constructor Function
+
+    this.markerList = ko.observableArray();
+
+    myMarkers.forEach(function(e){
+        console.log('Starting the creation of '+ e.name);
+        self.markerList.push(new Location(e));
     });
 
-    this.displayMarker = ko.computed(function() {
-        if(this.show() === true) {
-            this.marker.setMap(map);
+    // Creating a filtered list
+    // Kudos to the information found here
+    // http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
+
+    this.searchText = ko.observable("");
+
+    this.filteredList = ko.computed(function() {
+        var filter = self.searchText().toLowerCase();
+        if (self.searchText() === null) {
+            return self.markerList();
         } else {
-            this.marker.setMap(null);
+            return ko.utils.arrayFilter(self.markerList(), function(markerItem) {
+                if(markerItem.name().toLowerCase().indexOf(filter) >= 0) {
+                    markerItem.show(true);
+                    return true;
+                } else {
+                    markerItem.show(false);
+                    return false;
+                }
+            });
         }
-        return true;
-    }, this);
-};
+    }, self);
 
+}
 
-// View
-
-// Initialize the Markers on the Map
-
-function viewModel() {
+function initMap() {
     var self = this;
 
-    this.markersList = ko.observableArray([]);
-
     map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLong,
+        center: {lat: 30.2672, lng: -97.7431},
         zoom: 12
     });
 
-    var marker = new google.maps.Marker({
-        map: map,
-        position: myLatLong,
-        animation: google.maps.Animation.DROP,
-        title: "This is Austin, y'all!"
-    });
-
-    // myMarkers.forEach(function(marker) {
-    //     self.markersList.push( new markerItem(marker));
-    //     console.log(marker);
-    // })
-
-        myMarkers.forEach(function(locationItem){
-        self.myMarkers.push( new markerItem(locationItem));
-    });
-
+    ko.applyBindings(new ViewModel());
 };
-
-ko.applyBindings(viewModel());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
