877c08fd13ff201d2bb65b0a4df85e11eeceb3dc
Trey <trizney@austin.rr.com>
2016-10-08 02:25:23 -0500

    Moved Locations out of the ViewModel

--- js/app.js
+++ js/app.js
@@ -74,101 +74,107 @@ var myMarkers = [
     }
 ];
 
-// View Model
+
+// Model
+// Location Constructor Function
+var Location = function(data) {
+    var self = this;
+
+    console.log("Location "+data.name+" set");
+    this.name = ko.observable(data.name);
+        console.log(this.name());
+    this.lat = ko.observable(data.lat);
+        console.log(this.lat());
+    this.lng = ko.observable(data.lng);
+        console.log(this.lng());
+    this.street = ko.observable(data.street);
+        console.log(this.street());
+    this.city = ko.observable(data.city);
+        console.log(this.city());
+    this.phone = ko.observable(""); // Will be retrieved from Foursquare API
+        console.log(this.phone());
+    this.url = ko.observable("");  // Will be retrieved from Foursquare API
+        console.log(this.url());
+    this.show = ko.observable(true);
+        console.log(this.show());
+    //Items added from Foursquare API
+    // this.url = '';
+    //this.facebookName = ko.observable('');
+    //this.categoryName = ko.observable('');
+
+    // Foursquare API Information
+    var clientID = "D2GUCKJP4VMFZBHN3ZAL3WX2LNFDLARDD4HS5FG3IAJBRV4B";
+    var clientSecret = "2JUH3GZ3D1XDCG5R0WK5BPZHYAKYLNUCGXP4DY2ZOJWIIWO0";
+
+    // Foursquare API Call
+    // https://developer.foursquare.com/docs/venues/search
+    var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat() +
+    ',' + this.lng() + '&client_id=' + clientID + '&client_secret=' + clientSecret +
+    '&v=20160118' + '&query=' + this.name();
+    console.log(foursquareURL);
+
+    // .getJSON from each searched location
+    // http://api.jquery.com/jquery.getjson/
+    $.getJSON(foursquareURL, function(data) {
+        //console.log(data.repsonse.venues[0]);
+
+        var result = data.response.venues[0];
+        if (typeof self.url === 'undefined') {
+            self.url("");
+        } else {
+            self.url(result.url);
+        }
+        self.url(result.url);
+        console.log(self.url() + " This is the URL from Foursquare API");
+        self.phone(result.contact.formattedPhone);
+        console.log(self.phone() + " This is the formatted phone number from Foursquare API");
+
+        // that.facebookName(result.contact.facebookUsername);
+        // console.log(that.facebookName() + " This is the Facebook Name from Foursquare API")
+        // that.categoryName(result.categories.name);
+        // console.log(that.categoryName() + " This is the Category type from Foursquare API");
+
+    }).fail(function (){
+        alert("Danger, Will Robinson! Danger!");
+    });
+
+
+    // Google Maps API info window information
+    // https://developers.google.com/maps/documentation/javascript/infowindows
+    this.contentString = ko.computed(function() {
+        return  '<div><b>' + self.name() + '</b></div>' +
+                '<div>'+ self.street() + '</div>' +
+                '<div>'+ self.phone() + '</div>' +
+                '<div><a href="' + self.url() + '">' + self.url() + '</a></div>';
+    });
+
+    this.infoWindow = new google.maps.InfoWindow({
+        content: self.contentString()
+    });
+
+    this.marker = new google.maps.Marker({
+        position: new google.maps.LatLng(self.lat(), self.lng()),
+        map: map,
+        title: self.name()
+    });
+
+    this.marker.setMap(map);
+
+    this.marker.addListener('click', function() {
+        self.infoWindow.setContent(self.contentString());
+        self.infoWindow.open(map, this);
+    });
+}; // End of Location Constructor Function
+
+
+
+
+// ViewModel
 
 function ViewModel () {
 console.log("ViewModel Start");
 
     var self = this;
-
-    // Location Constructor Function
-    var Location = function(data) {
-        var that = this;
-        console.log("Location "+data.name+" set");
-        this.name = ko.observable(data.name);
-        console.log(this.name());
-        this.lat = ko.observable(data.lat);
-        console.log(this.lat());
-        this.lng = ko.observable(data.lng);
-        console.log(this.lng());
-        this.street = ko.observable(data.street);
-        console.log(this.street());
-        this.city = ko.observable(data.city);
-        console.log(this.city());
-        this.phone = ko.observable(""); // Will be retrieved from Foursquare API
-        console.log(this.phone());
-        this.url = ko.observable("");  // Will be retrieved from Foursquare API
-        console.log(this.url());
-        this.show = ko.observable(true);
-        console.log(this.show());
-        //Items added from Foursquare API
-        // this.url = '';
-        //this.facebookName = ko.observable('');
-        //this.categoryName = ko.observable('');
-
-        // Foursquare API Information
-        var clientID = "D2GUCKJP4VMFZBHN3ZAL3WX2LNFDLARDD4HS5FG3IAJBRV4B";
-        var clientSecret = "2JUH3GZ3D1XDCG5R0WK5BPZHYAKYLNUCGXP4DY2ZOJWIIWO0";
-
-        // Foursquare API Call
-        // https://developer.foursquare.com/docs/venues/search
-        var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat() +
-        ',' + this.lng() + '&client_id=' + clientID + '&client_secret=' + clientSecret +
-        '&v=20160118' + '&query=' + this.name();
-        console.log(foursquareURL);
-
-        // .getJSON from each searched location
-        // http://api.jquery.com/jquery.getjson/
-        $.getJSON(foursquareURL, function(data) {
-            //console.log(data.repsonse.venues[0]);
-
-            var result = data.response.venues[0];
-            if (typeof(result.url === 'undefined')) {
-                that.url("");
-            } else {
-                that.url(result.url);
-            }
-            that.url(result.url);
-            console.log(that.url() + " This is the URL from Foursquare API");
-            that.phone(result.contact.formattedPhone);
-            console.log(that.phone() + " This is the formatted phone number from Foursquare API");
-
-            // that.facebookName(result.contact.facebookUsername);
-            // console.log(that.facebookName() + " This is the Facebook Name from Foursquare API")
-            // that.categoryName(result.categories.name);
-            // console.log(that.categoryName() + " This is the Category type from Foursquare API");
-
-        }).fail(function (){
-            alert("Danger, Will Robinson! Danger!");
-        });
-
-
-        // Google Maps API info window information
-        // https://developers.google.com/maps/documentation/javascript/infowindows
-        this.contentString = ko.computed(function() {
-            return  '<div><b>' + that.name() + '</b></div>' +
-                    '<div>'+ that.street() + '</div>' +
-                    '<div>'+ that.phone() + '</div>' +
-                    '<div><a href="' + that.url() + '">' + that.url() + '</a></div>';
-        });
-
-        this.infoWindow = new google.maps.InfoWindow({
-            content: that.contentString()
-        });
-
-        this.marker = new google.maps.Marker({
-            position: new google.maps.LatLng(data.lat, data.lng),
-            map: map,
-            title: data.name
-        });
-
-        this.marker.setMap(map);
-
-        this.marker.addListener('click', function() {
-            that.infoWindow.setContent(that.contentString());
-            that.infoWindow.open(map, this);
-        });
-    }; // End of Location Constructor Function
 
     this.markerList = ko.observableArray();
 
